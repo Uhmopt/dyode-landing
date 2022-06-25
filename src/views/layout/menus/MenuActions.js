86c798b37b83337148fab3e87dd00f9ec7818a6c
Uhmopt <qethum@gmail.com>
2022-06-24 21:47:49 -0700

    [New] menu completed

--- src/views/layout/menus/MenuActions.js
+++ src/views/layout/menus/MenuActions.js
@@ -1,5 +1,18 @@
-import React from "react";
+import { Person, Search, ShoppingCart } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
 
 export default function MenuActions() {
-  return <div>MenuActions</div>;
+  return (
+    <div className="flex items-center">
+      {[
+        { icon: <Search /> },
+        { icon: <Person /> },
+        { icon: <ShoppingCart /> },
+      ].map((item, itemIndex) => (
+        <IconButton key={itemIndex} size="small" color="inherit">
+          {item.icon ?? null}
+        </IconButton>
+      ))}
+    </div>
+  );
 }
