3f3999a1479dbc60c0d581dcb9834ca0c3f9a3a2
Uhmopt <qethum@gmail.com>
2022-06-26 02:30:48 -0700

    [New] scroll header

--- src/views/layout/Header.js
+++ src/views/layout/Header.js
@@ -1,4 +1,4 @@
-import { AppBar, Container } from "@mui/material";
+import { AppBar, Container, Slide, useScrollTrigger } from "@mui/material";
 import Logo from "components/Logo";
 import DesktopMenu from "./menus/DesktopMenu";
 import MenuActions from "./menus/MenuActions";
@@ -6,19 +6,23 @@ import MobileMenu from "./menus/MobileMenu";
 import PreHeader from "./PreHeader";
 
 export default function Header() {
+  const trigger = useScrollTrigger({ target: window });
+
   return (
-    <AppBar>
-      <PreHeader />
-      <Container maxWidth="lg">
-        <div className="flex justify-between items-center py-4">
-          <div className="flex items-center">
-            <MobileMenu />
-            <Logo />
+    <Slide appear={false} direction="down" in={!trigger}>
+      <AppBar>
+        <PreHeader />
+        <Container maxWidth="lg">
+          <div className="flex justify-between items-center py-4">
+            <div className="flex items-center">
+              <MobileMenu />
+              <Logo />
+            </div>
+            <DesktopMenu />
+            <MenuActions />
           </div>
-          <DesktopMenu />
-          <MenuActions />
-        </div>
-      </Container>
-    </AppBar>
+        </Container>
+      </AppBar>
+    </Slide>
   );
 }
