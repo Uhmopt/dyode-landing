606eda3b8928fb9e61e27176d408c21551a8f0d8
Uhmopt <qethum@gmail.com>
2022-06-26 02:40:33 -0700

    [New] footer container

--- src/views/layout/footers/FooterMap.js
+++ src/views/layout/footers/FooterMap.js
@@ -1,61 +1,63 @@
 import { Facebook, Instagram } from "@mui/icons-material";
-import { Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import CustomTypo from "components/CustomTypo";
 import React from "react";
 import FooterMapList from "./FooterMapList";
 
 export default function FooterMap() {
   return (
-    <div className="py-16 sm-down:py-8 px-12 sm-down:px-10 bg-black text-white">
-      <Grid container justifyContent="space-between" spacing={4}>
-        <Grid item>
-          <Grid container spacing={2}>
-            <Grid item flexGrow={1}>
-              <div className="sm:mr-10">
-                <FooterMapList
-                  title="Customer Services"
-                  data={[
-                    { label: "Accessibility", to: "" },
-                    { label: "Contact Us", to: "" },
-                    { label: "Return Policy", to: "" },
-                    { label: "FAQ", to: "" },
-                    { label: "Gift Certificates", to: "" },
-                    { label: "Wishlist", to: "" },
-                  ]}
-                />
-              </div>
+    <div className="py-16 sm-down:py-8 bg-black text-white">
+      <Container>
+        <Grid container justifyContent="space-between" spacing={4}>
+          <Grid item>
+            <Grid container spacing={2}>
+              <Grid item flexGrow={1}>
+                <div className="sm:mr-10">
+                  <FooterMapList
+                    title="Customer Services"
+                    data={[
+                      { label: "Accessibility", to: "" },
+                      { label: "Contact Us", to: "" },
+                      { label: "Return Policy", to: "" },
+                      { label: "FAQ", to: "" },
+                      { label: "Gift Certificates", to: "" },
+                      { label: "Wishlist", to: "" },
+                    ]}
+                  />
+                </div>
+              </Grid>
+              <Grid item flexGrow={1}>
+                <div className="sm:mr-10">
+                  <FooterMapList
+                    title="Company"
+                    data={[
+                      { label: "About Us", to: "" },
+                      { label: "Careers", to: "" },
+                      { label: "Press", to: "" },
+                      { label: "Affiliates", to: "" },
+                    ]}
+                  />
+                </div>
+              </Grid>
             </Grid>
-            <Grid item flexGrow={1}>
-              <div className="sm:mr-10">
-                <FooterMapList
-                  title="Company"
-                  data={[
-                    { label: "About Us", to: "" },
-                    { label: "Careers", to: "" },
-                    { label: "Press", to: "" },
-                    { label: "Affiliates", to: "" },
-                  ]}
-                />
-              </div>
+          </Grid>
+          <Grid item>
+            <CustomTypo variant="h3">Follow Us</CustomTypo>
+            <Grid container justifyContent="space-between">
+              <Grid item>
+                <CustomTypo variant="h2">
+                  <Facebook fontSize="inherit" />
+                </CustomTypo>
+              </Grid>
+              <Grid item>
+                <CustomTypo variant="h2">
+                  <Instagram fontSize="inherit" />
+                </CustomTypo>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item>
-          <CustomTypo variant="h3">Follow Us</CustomTypo>
-          <Grid container justifyContent="space-between">
-            <Grid item>
-              <CustomTypo variant="h2">
-                <Facebook fontSize="inherit" />
-              </CustomTypo>
-            </Grid>
-            <Grid item>
-              <CustomTypo variant="h2">
-                <Instagram fontSize="inherit" />
-              </CustomTypo>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+      </Container>
     </div>
   );
 }
--- src/views/layout/footers/Subscribe.js
+++ src/views/layout/footers/Subscribe.js
@@ -1,39 +1,41 @@
 import { Email } from "@mui/icons-material";
-import { Button, Grid, Paper, TextField } from "@mui/material";
+import { Button, Container, Grid, Paper, TextField } from "@mui/material";
 import CustomTypo from "components/CustomTypo";
 import React from "react";
 
 export default function Subscribe() {
   return (
-    <div className="py-20 sm-down:py-8 px-12 sm-down:px-10 bg-primary text-white">
-      <CustomTypo variant="h1" className="pb-5 text-center">
-        <Email fontSize="inherit" /> Sign Up & Stay Connected
-      </CustomTypo>
-      <Grid container spacing={2} justifyContent="center">
-        <Grid item lg={6} md={6} sm={12} xs={12}>
-          <CustomTypo variant="body2 ">
-            Sign up for the newsletter and get 20% off! Gain access to exclusive
-            offers and be the first to know when new stuff drops!
-          </CustomTypo>
+    <div className="py-20 sm-down:py-8 bg-primary text-white">
+      <Container>
+        <CustomTypo variant="h1" className="pb-5 text-center">
+          <Email fontSize="inherit" /> Sign Up & Stay Connected
+        </CustomTypo>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item lg={6} md={6} sm={12} xs={12}>
+            <CustomTypo variant="body2 ">
+              Sign up for the newsletter and get 20% off! Gain access to
+              exclusive offers and be the first to know when new stuff drops!
+            </CustomTypo>
+          </Grid>
+          <Grid
+            item
+            lg={6}
+            md={6}
+            sm={12}
+            xs={12}
+            className="flex justify-center items-center"
+          >
+            <div className="flex-grow mr-4 sm-down:hidden">
+              <Paper>
+                <TextField fullWidth size="small" />
+              </Paper>
+            </div>
+            <Button variant="outlined" color="inherit" size="large">
+              SUBSCRIBE
+            </Button>
+          </Grid>
         </Grid>
-        <Grid
-          item
-          lg={6}
-          md={6}
-          sm={12}
-          xs={12}
-          className="flex justify-center items-center"
-        >
-          <div className="flex-grow mr-4 sm-down:hidden">
-            <Paper>
-              <TextField fullWidth size="small" />
-            </Paper>
-          </div>
-          <Button variant="outlined" color="inherit" size="large">
-            SUBSCRIBE
-          </Button>
-        </Grid>
-      </Grid>
+      </Container>
     </div>
   );
 }
